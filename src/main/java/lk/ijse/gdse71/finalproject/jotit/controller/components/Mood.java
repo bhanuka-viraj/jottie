855a0cce28cc612a724d0a993a6370c479c970f6
bhanuka-viraj <145824067+bhanuka-viraj@users.noreply.github.com>
2024-11-13 01:20:20 +0530

    moods added

--- src/main/java/lk/ijse/gdse71/finalproject/jotit/controller/components/Mood.java
+++ src/main/java/lk/ijse/gdse71/finalproject/jotit/controller/components/Mood.java
@@ -2,6 +2,7 @@ package lk.ijse.gdse71.finalproject.jotit.controller.components;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class Mood {
@@ -14,5 +15,19 @@ public class Mood {
 
     public void setLabel(String lbl){
         lblMood.setText(lbl);
+        // Load the corresponding image based on the label
+        // Load the corresponding image based on the label
+        String imagePath = "/view/assets/moodPics/" + lbl.toLowerCase() + ".png";
+        System.out.println(imagePath);
+        Image moodImage;
+        try {
+            moodImage = new Image(getClass().getResourceAsStream(imagePath));
+        }catch (Throwable e){
+            imagePath = "/view/assets/moodPics/default.png";
+            moodImage = new Image(getClass().getResourceAsStream(imagePath));
+        }
+
+        image.setImage(moodImage);
+
     }
 }
